e3f6edfd10a25fc544309f12201af0cd95f802d8
Jeffrey Gerard <IamJeffG@users.noreply.github.com>
2023-10-19 09:28:59 -0500

    Add README for httpserver
    [ci skip]

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
 
-# Ensure an up-to-date build of the auditor-core dependency that happens to live in this repo.
+# Ensure an up-to-date build of the gccd dependency that happens to live in this repo.
 # tox -e py310 --notest -c ./gccd_pkg
 (cd gccd_pkg ; python -m build)
-cp gccd_pkg/dist//gccd-*.tar.gz httpservice/gccd.tar.gz
+cp gccd_pkg/dist/gccd-*.tar.gz httpservice/gccd.tar.gz
 
 # Build docker image for batch jobs
 IMAGE_TAG=${1:-gccd}
