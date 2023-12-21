82c48f679404a9913bee5540a4a3e226b80eba51
Rudo Kemper <31662219+rudokemper@users.noreply.github.com>
2023-12-21 13:34:01 -0500

    Add a step to create JPG images from GeoTIFFs using gdal_translate (#19)
    * Generate JPGs from GeoTIFFs using gdal

--- gccd_pkg/gccd/utils.py
+++ gccd_pkg/gccd/utils.py
@@ -45,6 +45,29 @@ def copy_input_files(input_geojson_path, input_t0_path, input_t1_path, output_di
             print(f"\033[1m\033[31mError copying GeoTIFF files:\033[0m {e}")
             sys.exit(1)
 
+def generate_jpgs_from_geotiffs(input_t0_path, input_t1_path, output_directory, output_filename):
+    resources_dir = os.path.join(output_directory, "resources")
+    os.makedirs(resources_dir, exist_ok=True)
+
+    # Generate JPGs from GeoTIFFs
+    try:
+        # Determine the JPG output filenames
+        t0_output_filename = os.path.basename(f'{output_filename}_t0.jpg')
+        t1_output_filename = os.path.basename(f'{output_filename}_t1.jpg')
+        # Determine the full path to the JPG output files
+        t0_output_path = os.path.join(resources_dir, t0_output_filename)
+        t1_output_path = os.path.join(resources_dir, t1_output_filename)
+        # Generate the JPG files
+        subprocess.check_output(['gdal_translate', '-of', 'JPEG', input_t0_path, t0_output_path])
+        subprocess.check_output(['gdal_translate', '-of', 'JPEG', input_t1_path, t1_output_path])
+        # Delete the xml artifacts that gdal_translate generates
+        os.remove(f'{t0_output_path}.aux.xml')
+        os.remove(f'{t1_output_path}.aux.xml')
+        print(f"\033[1m\033[32mT0 and T1 JPG files generated and saved to:\033[0m {resources_dir}")
+    except subprocess.CalledProcessError as e:
+        print(f"\033[1m\033[31mAn error occurred trying to generate JPG files:\033[0m {e}")
+        sys.exit(1)
+
 def read_geojson_file(input_path):
     try:
         with open(input_path, 'r') as geojson_file:
